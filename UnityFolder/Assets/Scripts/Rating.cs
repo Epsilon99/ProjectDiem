1c38e4ed3bfb26addec95b64811a0bcec4088d3e
unknown <casp3930@VIDF0DEF1FC8055.VID.net.local>
2014-09-23 12:31:12 +0200

    Er begyndt på et rating system. Har lavet en GUI knap, som skaber 2 GUI knapper. En Like og en Dislike. Når man trykker på knapperne adder den til varibalerne til det enkelte billede.

---
+++ UnityFolder/Assets/Scripts/Rating.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rating : MonoBehaviour {
+
+    public GameObject likeDislike;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnGUI()
+    {
+        if (GUI.Button(new Rect(Screen.width / 2 + 25, 300, 50, 30), "Rating"))
+        {
+            Debug.Log("Clicked Rating");
+            Instantiate(likeDislike, new Vector2(0,0), Quaternion.identity);
+        }
+    }
+}
