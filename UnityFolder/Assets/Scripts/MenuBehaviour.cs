3b7428fc34e90262c8074efb2eea8d35736fcb47
Benjamin Holst <bnej4411@gmail.com>
2014-09-23 12:39:15 +0200

    Script for handling menues and the menues behaviour were added. (WIP). Cleaned up a bit in the assets. Added my own scene (mick), to edit in, so no scene conflict will happen.

---
+++ UnityFolder/Assets/Scripts/MenuBehaviour.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuBehaviour : MonoBehaviour {
+
+    public float speed;
+    private GameObject rightChecker;
+    private GameObject leftChecker;
+    private GameObject midChecker;
+    private Transform thisTransform;
+
+	// Use this for initialization
+	void Start () {
+        thisTransform = transform;
+        rightChecker = GameObject.FindGameObjectWithTag("RightChecker");
+        leftChecker = GameObject.FindGameObjectWithTag("LeftChecker");
+        midChecker = GameObject.FindGameObjectWithTag("MidChecker");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void SlideToTheSide(bool slideLeft) { 
+        float curSpeed;
+        Transform curTarget;
+
+        if (slideLeft){
+            curSpeed = -speed;
+            curTarget = leftChecker.transform;
+            while (thisTransform.position.x > curTarget.position.x) {
+                thisTransform.Translate(Vector2.right * curSpeed * Time.deltaTime);
+            }
+            gameObject.SetActive(false);
+        }
+        else{
+            curSpeed = speed;
+            curTarget = rightChecker.transform;
+            while (thisTransform.position.x < curTarget.position.x)
+            {
+                thisTransform.Translate(Vector2.right * curSpeed * Time.deltaTime);
+            }
+            gameObject.SetActive(false);
+        }
+    }
+
+    public void SlideToCenter(bool slideFromLeft) {
+        float curSpeed;
+        Transform curTarget = midChecker.transform;
+
+        if (slideFromLeft){
+            curSpeed = speed;
+            while (thisTransform.position.x < curTarget.position.x) {
+                thisTransform.Translate(Vector2.right * curSpeed * Time.deltaTime);
+            }
+        }
+        else{
+            curSpeed = -speed;
+            while (thisTransform.position.x > curTarget.position.x)
+            {
+                thisTransform.Translate(Vector2.right * curSpeed * Time.deltaTime);
+            }
+        }
+    }
+}
---
+++ UnityFolder/Assets/Scripts/MenuHandler.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuHandler : MonoBehaviour {
+
+    public GameObject curMenu;
+
+    public GameObject ChoosenScreen;
+
+    private bool areWeSwitching;
+    private bool isNewOnLeft;
+
+    private GameObject leftChecker;
+    private GameObject rightChecker;
+
+
+    public GameObject Menu2;
+    public GameObject Menu3;
+
+	// Use this for initialization
+	void Start () {
+        rightChecker = GameObject.FindGameObjectWithTag("RightChecker");
+        leftChecker = GameObject.FindGameObjectWithTag("LeftChecker");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if(Input.GetKeyDown(KeyCode.RightArrow)){
+            ChangeChoosenSreen(Menu2,true);
+        }
+
+        Controls();
+        
+	}
+
+    void Controls() {
+
+        if (areWeSwitching && ChoosenScreen != null) {
+            if (isNewOnLeft)
+            {
+                curMenu.GetComponent<MenuBehaviour>().SlideToTheSide(false);
+                ChoosenScreen.GetComponent<MenuBehaviour>().SlideToCenter(true);
+                curMenu = ChoosenScreen;
+                areWeSwitching = false;
+                ChoosenScreen = null;
+            }
+            else {
+                curMenu.GetComponent<MenuBehaviour>().SlideToTheSide(true);
+                ChoosenScreen.GetComponent<MenuBehaviour>().SlideToCenter(false);
+                curMenu = ChoosenScreen;
+                areWeSwitching = false;
+                ChoosenScreen = null;
+            }
+        }
+    }
+
+    public void ChangeChoosenSreen(GameObject menuToSwitchToo,bool isItLeft) {
+        if (isItLeft)
+        {
+            menuToSwitchToo.transform.position = leftChecker.transform.position;
+            isNewOnLeft = true;
+        }
+        else {
+            menuToSwitchToo.transform.position = rightChecker.transform.position;
+            isNewOnLeft = false;
+        }
+        ChoosenScreen = menuToSwitchToo;
+        menuToSwitchToo.SetActive(true);
+        areWeSwitching = true;
+    }
+}
