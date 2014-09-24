e925081718536e14682477dffcd8b5cb366cadb3
Mick Holst <mick.holst@gmail.com>
2014-09-24 18:13:43 +0200

    Fixed the Camera and added snapshot function.

---
+++ UnityFolder/Assets/Scripts/CameraButtonBehaviour.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraButtonBehaviour : MonoBehaviour {
+
+    public GameObject camScreen;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnPress(bool isDown)
+    {
+        if (isDown)
+        {
+            camScreen.GetComponent<CameraScript>().TakeSnapshot();
+        }
+
+    }
+}
---
+++ UnityFolder/Assets/Scripts/CameraScript.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraScript : MonoBehaviour {
+
+    public Texture defaultTexture;
+    public WebCamDevice[] devices;
+    public WebCamTexture webcamTexture;
+
+
+    private string directory;
+    int captureCounter = 0;
+
+    void Awake() {
+        directory = Application.persistentDataPath + "/pictures";
+    }
+
+    void OnEnable() {
+        if (webcamTexture != null)
+        {
+            renderer.material.mainTexture = webcamTexture;
+            webcamTexture.Play();
+        }
+
+    }
+
+    void OnDisable() {
+
+        webcamTexture.Stop();
+        renderer.material.mainTexture = defaultTexture;
+    }
+
+	// Use this for initialization
+	void Start () {
+        devices = WebCamTexture.devices;
+        webcamTexture = new WebCamTexture();
+
+        if (devices.Length > 0)
+        {
+            webcamTexture.deviceName = devices[0].name;
+            renderer.material.mainTexture = webcamTexture;
+            webcamTexture.Play();
+        }
+	}
+
+
+    public void TakeSnapshot()
+    {
+        Texture2D snap = new Texture2D(webcamTexture.width, webcamTexture.height);
+        snap.SetPixels(webcamTexture.GetPixels());
+        snap.Apply();
+
+        System.IO.File.WriteAllBytes(directory + "YourPicture" + captureCounter + ".png", snap.EncodeToPNG());
+        captureCounter++;
+    }
+}
